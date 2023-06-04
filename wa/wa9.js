d8e9c8816e0402fe57a44beae29fddd6a3ff3a0d
APAK14 <alpa1451@colorado.edu>
2023-06-04 16:05:27 -0600

    wa9

--- wa/wa9.js
+++ wa/wa9.js
@@ -7,15 +7,18 @@ const showAnswerButton = document.getElementById('js-show-answer');
 // Function to fetch a random trivia question
 function getQuestion() {
   // Make a fetch request to the trivia API endpoint
-  fetch('https://your-api-endpoint')
+  fetch('https://opentdb.com/api.php?amount=10&category=12')
     .then(response => response.json())
     .then(data => {
       // Extract the question and answer from the API response
-      const question = data.question;
-      const answer = data.answer;
+      const question = data.results[0].question;
+      const answer = data.results[0].answer;
+
+
 
       // Display the question in the HTML
-      questionElement.textContent = question;
+      questionElement.innerHTML = question;
+
 
       // Store the answer in a custom data attribute for later use
       questionElement.dataset.answer = answer;
@@ -46,3 +49,25 @@ showAnswerButton.addEventListener('click', showAnswer);
 
 // Fetch a new question when the page loads
 getQuestion();
+
+// Select the share button element
+const shareButton = document.getElementById('js-share');
+
+// Function to handle the share button click
+function handleShare() {
+  // Check if the Web Share API is supported by the browser
+  if (navigator.share) {
+    navigator.share({
+      title: 'Check out this trivia question!',
+      text: 'Can you answer this?',
+      url: window.location.href
+    })
+      .then(() => console.log('Shared successfully'))
+      .catch(error => console.error('Error sharing:', error));
+  } else {
+    alert('Web Share API is not supported in this browser.');
+  }
+}
+
+// Event listener for share button click
+shareButton.addEventListener('click', handleShare);
