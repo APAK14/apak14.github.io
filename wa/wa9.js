09bd703d8f63eb887a2c22ccee8875333255b554
APAK14 <alpa1451@colorado.edu>
2023-06-04 17:26:49 -0600

    answers

--- wa/wa9.js
+++ wa/wa9.js
@@ -1,3 +1,5 @@
+// Include the he.js library in your HTML file before the JavaScript code
+
 // Select elements from the HTML
 const questionElement = document.getElementById('js-question');
 const answerElement = document.getElementById('js-answer');
@@ -7,23 +9,27 @@ const showAnswerButton = document.getElementById('js-show-answer');
 // Function to fetch a random trivia question
 function getQuestion() {
   // Make a fetch request to the trivia API endpoint
-  fetch('https://opentdb.com/api.php?amount=10&category=12')
+  fetch('https://opentdb.com/api.php?amount=10')
     .then(response => response.json())
     .then(data => {
-      // Extract the question and answer from the API response
-      const question = data.results[0].question;
-      const answer = data.results[0].correct_answer;
+      // Extract the question and answer options from the API response
+      const question = he.decode(data.results[0].question);
+      const answerOptions = data.results[0].incorrect_answers;
+      const correctAnswer = data.results[0].correct_answer;
 
       // Display the question in the HTML
-      questionElement.innerHTML = question;
+      questionElement.textContent = question;
 
+      // Display the answer options
+      answerElement.innerHTML = '';
+      answerOptions.forEach(option => {
+        const optionElement = document.createElement('div');
+        optionElement.textContent = he.decode(option);
+        answerElement.appendChild(optionElement);
+      });
 
-      // Store the answer in a custom data attribute for later use
-      questionElement.dataset.answer = answer;
-
-      // Clear any previous answer from the display
-      answerElement.textContent = '';
-      answerElement.style.display = 'none';
+      // Store the correct answer for later use
+      questionElement.dataset.answer = he.decode(correctAnswer);
     })
     .catch(error => {
       console.error(error);
@@ -31,26 +37,17 @@ function getQuestion() {
     });
 }
 
+  
+
 // Function to show the answer
-    function showAnswer() {
-        // Show the loading text
-        const loadingElement = document.getElementById('js-loading');
-        loadingElement.style.display = 'block';
-      
-        // Get the answer from the stored data attribute
-        const answer = questionElement.dataset.answer;
-      
-        // Delay displaying the answer for demonstration purposes
-        setTimeout(() => {
-          // Display the answer in the HTML
-          answerElement.textContent = `Answer: ${answer}`;
-          answerElement.style.display = 'block';
-      
-          // Hide the loading text
-          loadingElement.style.display = 'none';
-        }, 2000); // Adjust the delay time as desired
-      }
-      
+function showAnswer() {
+  // Get the answer from the stored data attribute
+  const answer = questionElement.dataset.answer;
+
+  // Display the answer in the HTML
+  answerElement.textContent = `Answer: ${answer}`;
+  answerElement.style.display = 'block';
+}
 
 // Event listeners for button clicks
 newQuestionButton.addEventListener('click', getQuestion);
